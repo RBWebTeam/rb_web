fdd8a814da2b6cf6c81bb18821977049b3510bdc
manishdixit18 <manishdixit18@gmail.com>
2016-12-26 19:02:13 +0530

    services for fb

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,7 +9,8 @@ use DB;
 class ProfileController extends Controller
 {
     function my_profile(Request $req){
-    	if(Session::get('is_login')){
+    	if(Session::get('is_login'))
+    	{
         	return view('my-profile');
     	}else{
     		return redirect('/');

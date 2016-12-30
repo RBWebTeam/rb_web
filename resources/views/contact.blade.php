b3caee34e2d2e1a9c7eb1ee58f13715c86662df2
manishdixit18 <manishdixit18@gmail.com>
2016-12-30 20:31:07 +0530

    contact us resolved

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,49 +8,26 @@
 			<h1>Have us call you now. <span style="color:red">It's FREE!</span></h1>
 			<h2>Your contact details:</h2>
 			<div class="row">
-				<form class="col-sm-12 helpform" name="subscribe-form1" id="subscribe-form1" method="POST">
-					<div class="row">
-						<div class="form-group col-sm-6">
-						  <label for="usr">First Name:</label>
-						  <input type="text" class="form-control" id="fname" name="fname" required="">
-						</div>
-						<div class="form-group col-sm-6">
-						  <label for="usr">Last Name:</label>
-						  <input type="text" class="form-control" id="lname" name="lname" required="">
-						</div>
-					</div>
-					<div class="row">
-						<div class="form-group col-sm-6">
-						  <label for="usr">Mobile Number:</label>
-						  <input type="text" name="subscribe_mobile1" id="subscribe_mobile1" class="form-control" minlength="10" onkeypress="return fnAllowNumeric(event)" pattern="[789][0-9]{9}" required="" maxlength="10" placeholder="Contact">
-						</div>
-						<div class="form-group col-sm-6">
-						  <label for="usr">Select Product:</label>
-						  <select class="form-control" name="prod" id="prod" required="">
-			        <option value="">Loan</option>
-			        						
-					<option value="PERSONAL LOAN">Personal loan</option>
-											
-					<option value="HOME LOAN">Home loan</option>
-											
-					<option value="CAR LOAN">Car loan</option>
-											
-					<option value="BUSINESS LOAN">Business loan</option>
-											
-					<option value="SME">Sme</option>
-											
-					<option value="LOAN AGAINST PROPERTY">Loan against property</option>
-											
-					<option value="CREDIT CARD">Credit card</option>
-								      </select>
-						</div>
-					</div>
-					<div class="row">
-						<div class="form-group col-sm-6">
-						  <button type="button" id="subscribe-form-submit1" class="btn btn-danger btn-lg sidebar-submit" style="background-color:#ed1c24">CALL ME BACK NOW</button>
-						</div>
-					</div>
-				</form>
+				<form name="contact_back_now" id="subscribe-form" method="post">
+                   {{ csrf_field() }}
+                <input type="hidden" name="form" value="contact-back-form">
+                        
+                    <div class=" col-sm-offset-1 col-sm-4">
+                        <fieldset>
+                            <input class="newsletter-name" id="name" name="name" placeholder="Name" required>
+                        </fieldset>
+                        </div>
+                    <div class="col-sm-4">
+                        <fieldset>
+                            <input type="number" class="newsletter-name" name="contact" onkeypress="return fnAllowNumeric(event)" required placeholder="number">
+                        </fieldset>                                 
+                        </div>
+                    <div class="col-sm-2 btncent">
+                    <button class="btn btn-primary btn-outline with-arrow sidebar-submit" onclick="$('#err').show();">Call Me Back<i class="icon-arrow-right"></i></button>
+
+                    </div>
+            </form>
+            <span id='err' style="color: red; display: none;">Will reach you soon</span>
 			</div>
 		</div>
 	</div>

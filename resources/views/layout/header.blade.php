6cf9f774e36e4d9609fe304549c5f599e65040c5
manishdixit18 <manishdixit18@gmail.com>
2017-01-03 18:32:25 +0530

    autocommit module

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -63,7 +63,7 @@
 
                 <li><a href="{{URL::to('https://twitter.com/rupeeboss')}}" title="Twitter"><i class="icon-twitter"></i></a></li>
                 <li><a href="{{URL::to('https://www.facebook.com/rupeeboss')}}" title="Facebook"><i class="icon-facebook"></i></a></li>
-                <li><a href="{{URL::to('https://plus.google.com/112574970643410375765')}}" title="GooglePlus><i class="icon-google-plus"></i></a></li>
+                <li><a href="{{URL::to('https://plus.google.com/112574970643410375765')}}" title="GooglePlus"><i class="icon-google-plus"></i></a></li>
                 <li><a href="#" title="instagram"><i class="icon-instagram"></i></a></li>
                 <li><a href="{{URL::to('contact-us')}}"><i class="icon-phone"></i> 1800-267-629-6	</a></li>
 

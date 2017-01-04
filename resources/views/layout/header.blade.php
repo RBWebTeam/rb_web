46c13d3bcd95f2bfe00a29608a6cd2c620e68457
manishdixit18 <manishdixit18@gmail.com>
2017-01-04 18:23:23 +0530

    minor issue in dropdown

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -103,8 +103,8 @@
 						</li>
 						
 						<li><a href="{{URL::to('contact-us')}}">Credit Card</a></li>
-						<!-- <li><a href="#">Balance Transfer</a></li> -->
-						 <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" data-target="#">Balance Transfer</a></li>
+						
+						 <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" data-target="#">Balance Transfer</a>
 						 
 						 <ul class="dropdown-menu">
 						    <li>
@@ -112,7 +112,8 @@
 							<a href="{{URL::to('home-loan-transfer')}}">Personal Loan</a>
 							<a href="{{URL::to('home-loan-transfer')}}">Loan Against Property</a>
 							</li>
-							</ul>					   
+							</ul>	
+							</li>				   
 					
 
 							<?php if(Session::get('is_login')){

b59c3ec669878315c75cf89a77c4ecd9ad6d3def
manishdixit18 <manishdixit18@gmail.com>
2017-01-04 18:19:15 +0530

    removing conflict on city form

--- resources/views/personal-loan-test.blade.php
+++ resources/views/personal-loan-test.blade.php
@@ -68,10 +68,10 @@
 			</div>
 
 			<div class="col-md-12 text-center animate-box cont" style="display:none;" id="q9" >
-				<form action={{URL('search/autocomplete')}} method="POST" >
+				
 	             <h3>I have been living in
-				<input type="text" class="search_city input-pad" id='city_name' name='city_name' onkeyup="changeTest('city_name','step_3_btn')" placeholder='your city'>city</h3>
-				</form>
+				<input type="text" class="search_city input-pad" id='city_name' name='city_name' onfocus="changeTest('city_name','step_3_btn')" placeholder='your city'>city</h3>
+				
 			</div>
 			<div class="text-center" style="display: none;" id="step_3_btn">
 			<?php if(Session::get('is_login')) {?>

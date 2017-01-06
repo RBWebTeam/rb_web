3add5ca063072979f6d1589cc0d4a12f5705d66f
manishdixit18 <manishdixit18@gmail.com>
2017-01-06 12:10:28 +0530

    autocommit module

--- resources/views/layout/script.blade.php
+++ resources/views/layout/script.blade.php
@@ -1016,8 +1016,9 @@ $(document).ready(function(){
 	</body>
 </html>
 <!-- login Start-->
-    <div id="login_process" class="modal fade" role="dialog">
-  <div class="modal-dialog">
+
+<div id="login_process" class="modal fade" role="dialog">
+<div class="modal-dialog">
 
     <!-- Modal content-->
     <div class="modal-content">
@@ -1025,68 +1026,62 @@ $(document).ready(function(){
       
         <h4 class="modal-title">Fill details</h4>
       </div>
-      <div >
-     <div> 
+     
    <div id="send_otp">  
     <form class="form-horizontal" id="login_form_process" method="POST">
      {{ csrf_field() }}
    
     <div class="form-group">
     
-    <label for="name" class="col-sm-2 control-label">Name</label>
-    <div class="col-sm-10">
+    <label for="name" class="col-sm-3 control-label">Name</label>
+    <div class="col-sm-6">
       <input type="text" class="form-control" id="name" name="name" placeholder="your good name" required>
     </div>
   </div>
   <div class="form-group">
-    <label for="email" class="col-sm-2 control-label">Email</label>
-    <div class="col-sm-10">
+    <label for="email" class="col-sm-3 control-label">Email</label>
+    <div class="col-sm-6">
       <input type="email" class="form-control" id="email" name="email" placeholder="Email" required>
     </div>
   </div>
   <div class="form-group">
-    <label for="contact" class="col-sm-2 control-label">Contact</label>
-    <div class="col-sm-10">
+    <label for="contact" class="col-sm-3 control-label">Contact</label>
+    <div class="col-sm-6">
       <input type="text" class="form-control" id="contact" name="contact" placeholder="Contact number"  pattern="[789][0-9]{9}" required maxlength="10" minlength="10" onkeypress="return fnAllowNumeric(event)">
     </div>
   </div>
   <div class="form-group">
-     <label for="set_pwd" class="col-sm-2 control-label">Password</label>
-    <div class="col-sm-10">
+     <label for="set_pwd" class="col-sm-3 control-label">Password</label>
+    <div class="col-sm-6">
       <input type="password" class="form-control" id="set_pwd" name="set_pwd" placeholder="Password" required maxlength="10" minlength="6" >
       </div>
   </div>
   <div class="form-group">
-     <label for="set_pwd_confirm" class="col-sm-2 control-label">Password</label>
-    <div class="col-sm-10">
+     <label for="set_pwd_confirm" class="col-sm-3 control-label">Password</label>
+    <div class="col-sm-6">
       <input type="password" class="form-control" id="set_pwd_confirm" name="set_pwd_confirm" placeholder="Confirm Password" required maxlength="10" minlength="6" >
       </div>
   </div>
   <div class="form-group">
     
-    <div class="col-sm-10">
+    <div class="col-sm-6">
      <span id='msg_err' style="display: none;color:red;">oops something went wrong</span>
      <span id='pls_wait' style="display: none;color: red;">Please wait .....</span>
      <span id='pwd_match' style="display: none; color:red;">Password do not match.</span>
     </div>
 
   </div>
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-6">
+  <div class="form-group ">
+    <div class="col-sm-offset-2 col-sm-5">
     <input type="hidden" name="product" id="product_login" value="">
       <button class="btn btn-default"  id="send_otp_button"  >Send OTP</button>
     </div>
-    </div>
-    <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-6">
-    
+    <div class="col-sm-5">
       <a class="btn btn-default"  id="already_user" data-toggle="modal" data-target="#log_popup" data-dismiss="modal" >Already User</a>
     </div>
   </div>
   </form>
   </div>
-      </div>
-      </div>
       
     </div>
    

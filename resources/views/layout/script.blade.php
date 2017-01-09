f1457f460c684ce9837ea4f01e8ecd1fa86c78be
manishdixit18 <manishdixit18@gmail.com>
2017-01-09 17:08:39 +0530

    autocommit module

--- resources/views/layout/script.blade.php
+++ resources/views/layout/script.blade.php
@@ -1135,7 +1135,7 @@ $(document).ready(function(){
 <script type='text/javascript'>
 $(document).ready(function(){ 
     $(window).scroll(function(){ 
-        if ($(this).scrollTop() > 500) { 
+        if ($(this).scrollTop() > 400) { 
             $('#scroll').fadeIn(); 
         } else { 
             $('#scroll').fadeOut(); 

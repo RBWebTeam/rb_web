2723ac1621956b7a79f79f23f692ed5efbcabbbc
joeljangam <joeljangam146@gmail.com>
2017-01-09 17:03:13 +0530

    added scroll up function in script blade, css in style css

--- resources/views/layout/script.blade.php
+++ resources/views/layout/script.blade.php
@@ -1135,7 +1135,7 @@ $(document).ready(function(){
 <script type='text/javascript'>
 $(document).ready(function(){ 
     $(window).scroll(function(){ 
-        if ($(this).scrollTop() > 900) { 
+        if ($(this).scrollTop() > 500) { 
             $('#scroll').fadeIn(); 
         } else { 
             $('#scroll').fadeOut(); 

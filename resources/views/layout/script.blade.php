75053007faeff82e8383752d66b21169b4c72dff
manishdixit18 <manishdixit18@gmail.com>
2017-01-11 15:53:13 +0530

    autocommit module

--- resources/views/layout/script.blade.php
+++ resources/views/layout/script.blade.php
@@ -1272,6 +1272,10 @@ $('#step_3_btn').show();
 <!--scroll up function  -->
 <script type='text/javascript'>
 $(document).ready(function(){ 
+$('.close1').click(function(){
+  location.reload();
+});
+
     $(window).scroll(function(){ 
         if ($(this).scrollTop() > 400) { 
             $('#scroll').fadeIn(); 

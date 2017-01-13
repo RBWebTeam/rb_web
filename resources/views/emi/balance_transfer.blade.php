217da268218c45a080ba82c03ff8bb291ea33a51
gadesg666 <shekhar.gade@rupeeboss.com>
2017-01-13 17:37:03 +0530

    Changes in BT

--- resources/views/emi/balance_transfer.blade.php
+++ resources/views/emi/balance_transfer.blade.php
@@ -168,12 +168,12 @@ h4.great {
 }
 
 label {
-    color: #999;
-    font-size: 22px;
+    color: #fff;13px
+    font-size: ;
     font-weight: normal;
     position: absolute;
     pointer-events: none;
-    left: 15px;
+    left:10px;
     top: 1px;
     transition: all 0.2s ease;
     font-weight: 500;
@@ -469,26 +469,9 @@ label {
 		</script>
 	
 	<script src="js/classie.js"></script>
-		<script>
-		    var menuRight = document.getElementById( 'cbp-spmenu-s2' ),
-			 showRightPush = document.getElementById( 'showRightPush' ),
-			 body = document.body;
-
-			 showRightPush.onclick = function() {
-			 classie.toggle( this, 'active' );
-			classie.toggle( body, 'cbp-spmenu-push-toleft' );
-			classie.toggle( menuRight, 'cbp-spmenu-open' );
-			disableOther( 'showRightPush' );
-			};
-
-			function disableOther( button ) {
-				if( button !== 'showRightPush' ) {
-					classie.toggle( showRightPush, 'disabled' );
-				}
-			}
-		</script>
 		
-		<!--<script src="js/jQuery.min.js"></script>-->
+		
+		
 		<script src="js/bootstrap.min.js" type="text/javascript"></script>  
 		
 		<script>

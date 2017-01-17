1e48e4b5b6f217499901a94d5efc064a0d59d175
joeljangam <joeljangam146@gmail.com>
2017-01-17 19:16:10 +0530

    created lead_status func

--- resources/views/emi/switch_cal.blade.php
+++ resources/views/emi/switch_cal.blade.php
@@ -38,7 +38,7 @@
 foreach ($data as $key => $value){?>
 <tr>
 
-	<td><img src="{{ $value['Bank_Logo']}}" class="img-responsive">{{ $value['Bank_Name']}}</td>
+	<td><img src="{{ $value['Bank_Logo']}}" class="img-responsive"></td>
     <td>{{ $value['roi']}}</td>
     <td>{{ $value['pf']}} &nbsp; ({{ $value['pf_type']}})</td>
     

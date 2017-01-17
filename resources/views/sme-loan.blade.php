132959b66f46f5586f50f5065b7b180396271edc
gadesg666 <shekhar.gade@rupeeboss.com>
2017-01-17 14:21:50 +0530

    changes for style

--- resources/views/sme-loan.blade.php
+++ resources/views/sme-loan.blade.php
@@ -13,27 +13,20 @@
   <ol class="carousel-indicators">
     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
     <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-    <li data-target="#myCarousel" data-slide-to="3"></li>
+    
   </ol>
 
   <!-- Wrapper for slides -->
 <div class="carousel-inner" role="listbox">
     <div class="item active">
-      <img src="{{URL::to('images/loan.jpg')}}" alt="">
+      <img src="{{URL::to('images/01/sme.jpg')}}" alt="">
     </div>
 
     <div class="item">
-      <img src="{{URL::to('images/loan.jpg')}}" alt="">
+      <img src="{{URL::to('images/01/sme.jpg')}}" alt="">
     </div>
 
-    <div class="item">
-      <img src="{{URL::to('images/loan.jpg')}}" alt="">
-    </div>
-
-    <div class="item">
-      <img src="{{URL::to('images/loan.jpg')}}" alt="">
-    </div>
+    
   </div>
   <!-- Left and right controls -->
   <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
@@ -49,41 +42,51 @@
 			
 	  	</div>
 
-		<div class="col-md-4 red-bg1">
-	   <div class="form-in">
+		
+	
+	
+	<div class="col-md-4 red-bg1 ">
+	   <div class="form-in ">
 
-              <span class="head-1 heading-bmar">Request A Call Back<span><br>
-             <form class="" id="compareform" role="form" method="POST" action="customer_form.php">
-				  <div class="form-group">
-				  
+              <span class="head-1 heading-bmar">Request A Call Back</span>
+              <form class="" id="compareform" role="form" method="POST">
+            
+              {{ csrf_field() }}
+                <input type="hidden" name="form" value="subscribe-form">
+				  <div class="has-error">
 				    <div>
-				      <input type="text" class="form-control" id="name" name="name" placeholder="Name" required class="clr-ddd" />
+				      <input type="text" class="form-control" id="name" name="name" placeholder="Name" required />
+				       <input type="hidden" class="form-control" id="product" value="sme" name="product"  />
 				    </div>
 				  </div>
-			<div class="form-group">
+			<div class="has-error">
 		   
 		    <div> 
-		    <input type="text" name="contact" id="contact" class="form-control" 
-			pattern="[789][0-9]{9}" required maxlength="10" placeholder="Mobile No." class="clr-ddd">
+		    <input type="text" name="contact" id="contact" class="form-control clr-ddd" onkeypress="fnAllowNumeric(event)" 
+			pattern="[789][0-9]{9}" required maxlength="10" placeholder="Mobile No." >
 		    </div>
 		  </div>
-				 <div class="form-group">
+				 <div class="has-error">
 				  
 				    <div>
-				      <input type="email" class="form-control" id="email" name="email" placeholder="Email Id" required class="clr-ddd" />
+				      <input type="email" class="form-control clr-ddd" id="email" name="email" placeholder="Email Id" required  />
                  
 				    </div>
 				  </div>
 			 
 			  
-			  <div class="form-group centr-obj"> 
+			  <div class="centr-obj"> 
 			    
-			     <button class="btn btn-primary btn-outline with-arrow sidebar-submit">Submit<i class="icon-arrow-right"></i></button>
+			    <button class="btn btn-primary btn-outline with-arrow sidebar-submit" >Submit<i class="icon-arrow-right"></i></button>
 			   
 			  </div>
 			</form>
 		</div>	
+		<div class='msg' style="display: none;"><p>Thanks. We will reach you soon.</p></div>
+            <div class='msg_err' style="display: none;"><p>Ooops. Something went wrong.</p></div>
 	</div>
+	
+	
 	</aside>
 	</div>
 	

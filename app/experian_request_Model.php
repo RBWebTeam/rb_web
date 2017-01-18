f7fe6d9c18485ce4fdc08aff8b4366d7426d3cb7
manishdixit18 <manishdixit18@gmail.com>
2017-01-18 17:38:46 +0530

    autocommit module

--- app/experian_request_Model.php
+++ app/experian_request_Model.php
@@ -1,23 +1,20 @@
 <?php
 
 namespace App;
-
+use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Model;
-
-class experian_request extends Model
+use DB;
+class experian_request_model extends Model
 {
-
-	
-     protected $table='experian_request';
-     protected $fillable = [`id`, `clientName`, `hitId`, `voucherCode`, `firstName`, `middleName`, `surName`, `dob`, `gender`, `flatPlotHouseNo`, `society`, `locality`, `city`, `state`, `pincode`, `mobileNo`, `telephoneNo`, `telephoneType`, `email`, `pan`, `passport`, `aadhaar`, `voterid`, `driverLicenseNo`, `rationcard`, `created_at`, `updated_at`];
-     // public $timestamps = false;
-     public function store(Request $request)
+    //
+    protected $table='experian_request';
+    protected $fillable = ['clientName', 'hitId', 'voucherCode', 'firstName', 'middleName', 'surName', 'dob', 'gender', 'flatPlotHouseNo', 'society', 'locality', 'city', 'state', 'pincode', 'mobileNo', 'telephoneNo', 'telephoneType', 'email', 'pan', 'passport', 'aadhaar', 'voterid', 'driverLicenseNo', 'rationcard', 'created_at', 'updated_at'];
+    public function store(Request $request)
     {
-        $table = new experian_request();
+        $table = new experian_request_model();
         $input = $request->all();
-   		$id = $table::create($input)->id;
+         $id = $table::create($input)->id;
 
-   		return $id;
+         return $id;
     }
-
 }

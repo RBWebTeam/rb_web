556b94d73879264ce7c35610dee74cd822775c97
manishdixit18 <manishdixit18@gmail.com>
2017-01-24 10:32:35 +0530

    autocommit module

--- resources/views/doc_upload.blade.php
+++ resources/views/doc_upload.blade.php
@@ -12,26 +12,6 @@
   <div class="panel-body">
 Please Upload below documents :
 Identity Proof
-
-	  	@if (count($errors) > 0)
-			<div class="alert alert-danger">
-				<strong>Whoops!</strong> There were some problems with your input.<br><br>
-				<ul>
-					@foreach ($errors->all() as $error)
-						<li>{{ $error }}</li>
-					@endforeach
-				</ul>
-			</div>
-		@endif
-
-		@if ($message = Session::get('success'))
-		<div class="alert alert-success alert-block">
-			<button type="button" class="close" data-dismiss="alert">×</button>
-		        <strong>{{ $message }}</strong>
-		</div>
-		<img src="/images/{{ Session::get('path') }}">
-		@endif
-
 		<form action="{{ url('doc-upload') }}" enctype="multipart/form-data" method="POST">
 			{{ csrf_field() }}
 			<div class="row form-group">

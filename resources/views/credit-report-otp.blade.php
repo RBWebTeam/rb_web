7c0e2340040f23e0aa6243f42c32ab169f6cd601
manishdixit18 <manishdixit18@gmail.com>
2017-01-31 14:52:44 +0530

    autocommit module

--- resources/views/credit-report-otp.blade.php
+++ resources/views/credit-report-otp.blade.php
@@ -9,17 +9,21 @@
 					   <form name="credit_report_otp_form" id="credit_report_otp_form" method="POST">
 					       {{ csrf_field() }}
 					   <h3>Enter your mobile number & verify using OTP</h3>
-					   <input type="text" name="contact" id="mobile" class="form-control" pattern="[789][0-9]{9}" required maxlength="10" placeholder="9XXXX XXXXX">
+					   <input type="text" name="contact" id="mobile" class="form-control" pattern="[789][0-9]{9}" required maxlength="10" placeholder="9XXXX XXXXX" onkeypress="return fnAllowNumeric(event)">
 					   <hr class="hr-clr">
 					   <a class="btn btn-primary btn-lg btn-view" id="credit_report_send_otp">SEND OTP</a>
+					   <div id="mobile_val" style="display: none;color: red;">Phone number should be of 10 digits.</div>
 					   <div id="wait_div" style="display: none;color: red;">Sending OTP ...</div>
 					   </form>
+
+
 					   <form name="credit_report_verify_form" id="credit_report_verify_form" style="display:none" method="POST" onkeypress="return fnAllowNumeric(event)">
 					       {{ csrf_field() }}
 					   <h3>Enter your verification code sent on your number</h3>
-					   <input type="text" name="verify" id="verify" class="form-control" onkeypress="return fnAllowNumeric(event)" required maxlength="10" placeholder="verify otp">
+					   <input type="text" name="verify" id="verify" class="form-control" onkeypress="return fnAllowNumeric(event)" required maxlength="6" placeholder="verify otp">
 					   <hr class="hr-clr">
 					   <a class="btn btn-primary btn-lg btn-view" id="credit_report_verify_otp">VERIFY OTP</a>
+					   <div id="otp_val" style="display: none;color: red;">Please wait ...</div>
 					   <div id="wait_div_otp" style="display: none;color: red;">Please wait ...</div>
 					   </form>
 					   </div>
@@ -72,6 +76,13 @@ THE CREDIT INFORMATION SHALL BE SHARED BY EXPERIAN WITH YOU AND RUPEEBOSS ON A M
 @include('layout.script')
 <script type="text/javascript">
 	$('#credit_report_send_otp').click(function(){
+		console.log($('#mobile').val().length);
+		if(($('#mobile').val().length)<10){
+				$('mobile_val').show();
+				return false;
+		}
+
+			
 		$('#wait_div').show();
 		$.ajax({  
 	       type: "POST",  
@@ -90,6 +101,10 @@ THE CREDIT INFORMATION SHALL BE SHARED BY EXPERIAN WITH YOU AND RUPEEBOSS ON A M
 	});
 
 	$("#credit_report_verify_otp").click(function(){
+		if($('#verify').val().length<6)
+			{	$('otp_val').show();
+				return false;
+			}
 		$('#wait_div_otp').show();
 		$.ajax({  
 	       type: "POST",  

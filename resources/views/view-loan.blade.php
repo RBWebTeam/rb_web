939f9ae30353119099a472f2c81186a545d4ff31
durga pratap <scriptdp@gmail.com>
2017-02-01 11:45:20 +0530

    compare page change

--- resources/views/view-loan.blade.php
+++ resources/views/view-loan.blade.php
@@ -17,57 +17,40 @@
    
   
 
-  <tr>
-    <!-- <td width="9%" class="upper">Compare</td> -->
+  <tr>    
     <td class="bdr-btm"><center><img src="{{ $value->Bank_Logo}}" width="130" ></center></td>
-    <td width="16%" class="upper bdr-btm">Interest Rate</td>
-    <td width="16%" class="upper bdr-btm">Loan Amount</td>
-    <td width="18%" class="upper bdr-btm">emi</td>
+    <td width="16%" class="upper bdr-btm">Loan Tenure</td>
+    <td width="16%" class="upper bdr-btm">Processing Fee</td>
+    <td width="18%" class="upper bdr-btm">Interest Rate</td>
     <?php $Loans=strtolower(str_replace(' ','-',$home_loan)); ?>
-     
      <?php if($Loans=='lap'){?>
-
     <td width="17%" class="blue-bg"><h4><a href="{{url('apply-loan-against-property')}}" class="apply-btn">Apply Online</h4></a></td>
-
     <?php }else{?>
    <td width="17%" class="blue-bg"><h4><a href="{{url('apply-')}}<?php echo $Loans?>" class="apply-btn">Apply Online</a></h4></td>
     <?php }?>
   </tr>
 
   <tr>
-<!--     <td><input type="checkbox" name="cehckbox"/></td> -->
-    <td>{{$home_loan}}</td>
-    <td>{{$value->roi}}%</td>
-    <td>{{$value->amt_from}}-{{$value->amt_to}}</td>
-    <td>10,000 (for 3 years)</td>
-    <td rowspan="2"><strong>Special Features:-</strong> Pre close Fee 0%</td>
+     <td class="upper">  <button type="button" data-toggle="collapse" data-target="#{{$key}}">Know More</button></td>
+     <td>{{$value->Min_Tenure}}-{{$value->Max_Tenure}}</td>
+     <td>{{$value->pf}}</td>
+     <td>{{$value->roi}}%</td>
+     <td></td>
   </tr>
-    <tr>
-  <!--   <td><i class="icon-thumbs-up"></i></td> -->
-    <td class="upper">  <button type="button" data-toggle="collapse" data-target="#{{$key}}">Know More</button><!-- <a href="#">Know More</a> --></td>
-    <td>Processing Fee - INR {{$value->pf}} + ST</td>
-    <td>Interest Paid - INR 50,000</td>
-    <td>Max tenure - {{$value->Max_Tenure}}&nbsp;Years</td>
-    </tr>
+
+  
 
     
       
  <table>
-<div id="{{$key}}" class="collapse" style="background-color: #F6F6F6; width: 100%;height: auto;">
-   
+<div id="{{$key}}" class="collapse" style="background-color: #F6F6F6; width: 100%;height: auto;"> 
   <li class="list-group-item"><i class="fa fa-dot-circle-o" aria-hidden="true"></i> No guarantor required</li>
           <li class="list-group-item"><i class="fa fa-dot-circle-o" aria-hidden="true"></i> No pre-closure and part payment fee
             <mark style="color: red">Special offers are available for women applicants at a lower rate</mark></li>
           <li class="list-group-item"><i class="fa fa-dot-circle-o" aria-hidden="true"></i> Age at least 18 and less than 60</li>
            <li class="list-group-item"><i class="fa fa-dot-circle-o" aria-hidden="true"></i> Salaried / Self - Employed with regular income,Earn more than the minimum income required</li>
   </div>
-
    </table>
-
-
-
-
-
 </table>
 </div>
 @endforeach

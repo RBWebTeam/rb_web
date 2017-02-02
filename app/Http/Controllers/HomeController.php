7748672a7562d6d895d27ef985d4244cbf29e1e9
joeljangam <joeljangam146@gmail.com>
2017-02-02 17:38:55 +0530

    created loader

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,14 +9,15 @@ use DB;
 class HomeController extends Controller
 {
 	public function index(){
-		$data['city'] = DB::table('city_master')->select('City_Name','state_id','City_Id')->get();
-		$data['loan'] = DB::table('product_master')->select('Product_Name','Product_Id')->get();
+		$keywords='Home Loan,Business Loan,Personal Loan,Car Loan,Express Loan,Savings Account,Loan Against Property,Small and Medium Size Enterprise Loan,SME Finance,Retail Loan';
+		// $data['city'] = DB::table('city_master')->select('City_Name','state_id','City_Id')->get();
+		// $data['loan'] = DB::table('product_master')->select('Product_Name','Product_Id')->get();
 		//print "<pre>";
 		//print_r($data);
 		// $data['proc_city']=DB::select("call  usp_get_bank_quot (6000000,15,2900000,'M',100000,20000,'1985-12-12','N',0,0,10000000,500000,50000,500000,2)");
 		// print "<pre>";
 		 //print_r($data['proc_city']);exit();
-	    return view('index')->with($data);
+	    return view('index')->with('keywords',$keywords);
 	}
 	public function footercontent($id){
 		$data['bank_name'] = $id;
--- routes/web.php
+++ routes/web.php
@@ -105,4 +105,4 @@ Route::get('went-wrong','ErrorController@went_wrong');
 // Creditcardform
 Route::get('credit-card-form','CreditcardController@credit_card_form');
 Route::post('credit-submit','CreditcardController@credit_form_submit');
-// Route::post('credit-submit','CreditcardController@store');
+

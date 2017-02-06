2465ddb9bcb543c6e9da3824ad664e16dcbdf1ae
manishdixit18 <manishdixit18@gmail.com>
2017-02-06 12:24:26 +0530

    autocommit module

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -14,17 +14,27 @@ class UploadController extends Controller
 
     public function UploadPost(Request $request)
     {
+        try{
     	$this->validate($request, [
             'Identity_Proof' => 'required',
             'Income_Proof' => 'required',
             'Address_Proof' => 'required'
         ]);
          $imageName = time().'.'.$request->Identity_Proof->getClientOriginalExtension();
-         $type=array(($request->Identity_Proof));
-         print_r($type);exit();
-        $request->Identity_Proof->move(public_path('Upload'), $imageName);
-         $post_data='{"mobNo":"'.$req['contact'].'","msgData":"your otp is '.$otp.' - RupeeBoss.com",
-                    "source":"WEB"}';
+       
+      // print_r($request->Identity_Proof->getClientMimeType());
+       // print_r($request->Identity_Proof->getClientOriginalName());
+       // print_r($request->Identity_Proof->getpathName());
+        $filepath = $request->Identity_Proof->getpathName();//Image path
+        $file =fopen($filename, "r");
+       $contents = file_get_contents($filepath);
+       print_r($contents);exit();
+       
+
+       // $request->Identity_Proof->move(public_path('Upload'), $imageName);
+        $post_data='{"docType":"Identity_Proof","docextension":"jpeg",
+                    "refFBAId":"WEB","bytes":'.$contents.'}';
+                    //print_r($post_data);
                 $url = "http://beta.services.rupeeboss.com/LoginDtls.svc/xmlservice/uploadCustLoanDoc";
                 $ch = curl_init();
                 curl_setopt($ch, CURLOPT_VERBOSE, 1);
@@ -42,8 +52,10 @@ class UploadController extends Controller
                 
                 curl_close($ch);
 
-
-
+                return ($http_result);
+            }catch(Exception $ee){
+                return $ee;
+            }
        // print "<pre>";
     	//print_r($request->all());
     	//print_r($imageName);

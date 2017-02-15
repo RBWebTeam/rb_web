4eed00d5f14ea5caf5ded008f86d97a209ed80b8
manishdixit18 <manishdixit18@gmail.com>
2017-02-15 17:58:37 +0530

    resolving timezone issue + experian logging

--- app/experian_request_Model.php
+++ app/experian_request_Model.php
@@ -8,7 +8,7 @@ class experian_request_model extends Model
 {
     //
     protected $table='experian_request';
-    protected $fillable = ['clientName', 'hitId', 'voucherCode', 'firstName', 'middleName', 'surName', 'dob', 'gender', 'flatPlotHouseNo', 'society', 'locality', 'city', 'state', 'pincode', 'mobileNo', 'telephoneNo', 'telephoneType', 'email', 'pan', 'passport', 'aadhaar', 'voterid', 'driverLicenseNo', 'rationcard', 'created_at', 'updated_at'];
+    protected $fillable = ['clientName', 'hitId', 'voucherCode', 'firstName', 'middleName', 'surName', 'dob', 'gender', 'flatPlotHouseNo', 'society', 'locality', 'city', 'state', 'pincode', 'mobileNo', 'telephoneNo', 'telephoneType', 'email', 'panNo', 'passport', 'aadhaar', 'voterid', 'driverLicenseNo', 'rationcard', 'created_at', 'updated_at'];
     public function store(Request $request)
     {
         $table = new experian_request_model();

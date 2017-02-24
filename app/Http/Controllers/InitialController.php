77599d62c0ce003d9ce90823fad543eafd9f9fba
manishdixit18 <manishdixit18@gmail.com>
2017-02-24 16:47:30 +0530

    Auto commit module

--- app/Http/Controllers/InitialController.php
+++ app/Http/Controllers/InitialController.php
@@ -7,10 +7,12 @@ use Response;
 class InitialController extends Controller
 {
 	//public static $pro_code = array('huii' =>213 );
-	static function prod(){
+	static final function prod(){
 		$pro_code=DB::table('product_master')
       ->select('Product_Id','Product_Name')
       ->get();
       return $pro_code;
+      //use as
+      //$product_list=InitialController::prod();
 	}
 }

42045f4213842bac26b2a3d9d54c30a06fd0d227
manishdixit18 <manishdixit18@gmail.com>
2017-03-07 17:25:34 +0530

    Auto commit module

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -481,14 +481,16 @@ run_else:
 	public function getQuoteByBrokerId(Request $req){
 		//print_r($req['BrokerId']);exit();
 		$id=$req['BrokerId'];
+		$ProductId=$req['ProductId'];
+		$flag=$req['flag'];
 		 $data=DB::table('bank_quote_api_request')
 		 ->select('ID','ApplicantNme','LoanRequired','ApplicantIncome','Turnover','status','ProductId')
         ->where('BrokerId','=',$id)
-        ->where('ProductId','=','12')
+        ->where('ProductId','=',$ProductId)
         ->get();
         //calling Erp api
        			$emp_code=$req['empCode'];
-        		$post_data='{"brokerId":'.$id.',"empCode":"'.$emp_code.'"}';
+        		$post_data='{"brokerId":'.$id.',"empCode":"'.$emp_code.'","flag":"'.$flag.'","ProductId":"'.$ProductId.'"}';
         		//print_r($post_data);exit();
                  // $url="http://beta.services.rupeeboss.com/LoginDtls.svc/xmlservice/dsplyHomePersonalLoanAppDtls";
         		$url="http://services.rupeeboss.com/LoginDtls.svc/xmlservice/dsplyHomePersonalLoanAppDtls";
@@ -535,15 +537,17 @@ run_else:
 	public function getPersonalLoanQuoteByBrokerId(Request $req){
 		//print_r($req['BrokerId']);exit();
 		$id=$req['BrokerId'];
+		$ProductId=$req['ProductId'];
 		 $data=DB::table('bank_quote_api_request')
 		 ->select('ID','ApplicantNme','LoanRequired','ApplicantIncome','Turnover','status','ProductId')
         ->where('BrokerId','=',$id)
-        ->where('ProductId','=','9')
+        ->where('ProductId','=',$ProductId)
         ->get();
         //calling Erp api
        			$flag=$req['flag'];
         		$emp_code=$req['empCode'];
-        		$post_data='{"brokerId":'.$id.',"empCode":"'.$emp_code.'","flag":"'.$flag.'"}';
+
+        		$post_data='{"brokerId":'.$id.',"empCode":"'.$emp_code.'","flag":"'.$flag.'","ProductId":"'.$ProductId.'"}';
         		//print_r($post_data);exit();
                  // $url="http://beta.services.rupeeboss.com/LoginDtls.svc/xmlservice/dsplyPersonalLoanAppDtls";
         		$url="http://services.rupeeboss.com/LoginDtls.svc/xmlservice/dsplyPersonalLoanAppDtls";

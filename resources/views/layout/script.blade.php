293dc1f999726a24828ff03058566c61917b32d8
joeljangam <joeljangam146@gmail.com>
2017-03-10 15:05:25 +0530

    changes on modal

--- resources/views/layout/script.blade.php
+++ resources/views/layout/script.blade.php
@@ -2145,15 +2145,7 @@ function SetSession_pop(cookieName,cookieValue){
   
   <script type="text/javascript">
 	$(document).ready(function(){
-		if(localStorage.getItem('popState') != 'shown'){
-        $j("#popup").delay(2000).fadeIn();
-        localStorage.setItem('popState','shown')
-    }
-
-    $j('#popup-close, #popup').click(function(e) // You are clicking the close button
-    {
-        $j('#popup').fadeOut(); // Now the pop up is hiden.
-    });
+		$("#popup").modal('show');
 	});
 </script>
    <!-- emp login end -->

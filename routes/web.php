c358b5b4ced3d3b611dcef4c0bb6b1c02e405efa
gadesg666 <shekhar.gade@rupeeboss.com>
2017-03-16 16:11:50 +0530

    pull data

--- routes/web.php
+++ routes/web.php
@@ -204,6 +204,7 @@ Route::get('comfy','ComfyController@comfy');
 
 /**  idfc */
 Route::get('idfc','IdfcController@idfc');
+
 /**  tribe */
 Route::get('tribe','TribeController@tribe');
 Route::post('save-tribe-form','TribeController@save_tribe_form');

067c7fb5c23494ce4406993c16a9c4d20dd4d39f
joeljangam <joeljangam146@gmail.com>
2017-03-20 13:33:41 +0530

    changes on modal

--- routes/web.php
+++ routes/web.php
@@ -103,7 +103,7 @@ Route::post('express-loan-verify','LoanController@express_verify_otp');
 Route::post('aditya-express-loan','LoanController@express_form');
 Route::get('new-personal-loan','ContactController@new_personal_loan');
 Route::post('new-personal-loan','ContactController@new_personal_loan_register');
-Route::get('magic-link','ContactController@magic_link');
+Route::get('sitemap','ContactController@magic_link');
 
 
 //new loans test

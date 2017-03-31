78e6ae4666af3e6833d3a3d8ff265ab2c308199a
manishdixit18 <manishdixit18@gmail.com>
2017-03-31 12:54:20 +0530

    Auto commit module

--- app/Http/Controllers/tribeController.php
+++ app/Http/Controllers/tribeController.php
@@ -31,40 +31,48 @@ class tribeController extends CallApiController
 
 	//company_address
 	// doc_pan doc_aadhar doc_dl doc_passport doc_voter doc_electricity_bill doc_leave_license doc_reg_certification doc_tax_registration doc_comapny_it_returns doc_company_pan doc_vat_return doc_it_returns doc_other		
-			$documents_array = array('docpan','doc_aadhar','doc_dl','doc_passport','doc_voter','doc_electricity_bill','doc_leave_license','doc_reg_certification','doc_tax_registration','doc_comapny_it_returns','doc_company_pan','doc_vat_return','doc_it_returns','doc_other');
-	        $request=$req;
-	        $response=0;$i=0;
-	        for( $i=0;$i<sizeof($documents_array);$i++){
-                $str=$documents_array[$i];
-                if(! $req->$str){
-                	continue;
-                }               
-	                $imageName = time().'.'.$req->$str->getClientOriginalExtension();
-	                $extension=$req->$str->getClientOriginalExtension();
-	                $filename = $req->$str->getpathName();//Image path
-	                $file =fopen($filename, "rb");
-	                $contents = fread($file, filesize($filename));
-	                $base64=base64_encode($contents);
-	               	break;
-	                // $url = "http://beta.services.rupeeboss.com/LoginDtls.svc/xmlservice/uploadCustLoanDoc";
-				}							                      
+			// $documents_array = array('docpan','doc_aadhar','doc_dl','doc_passport','doc_voter','doc_electricity_bill','doc_leave_license','doc_reg_certification','doc_tax_registration','doc_comapny_it_returns','doc_company_pan','doc_vat_return','doc_it_returns','doc_other');
+	  //       $request=$req;
+	  //       $response=0;$i=0;
+	  //       for( $i=0;$i<sizeof($documents_array);$i++){
+   //              $str=$documents_array[$i];
+   //              if(! $req->$str){
+   //              	continue;
+   //              }               
+	  //               $imageName = time().'.'.$req->$str->getClientOriginalExtension();
+	  //               $extension=$req->$str->getClientOriginalExtension();
+	  //               $filename = $req->$str->getpathName();//Image path
+	  //               $file =fopen($filename, "rb");
+	  //               $contents = fread($file, filesize($filename));
+	  //               $base64=base64_encode($contents);
+	  //              	break;
+	  //               // $url = "http://beta.services.rupeeboss.com/LoginDtls.svc/xmlservice/uploadCustLoanDoc";
+			// 	}				
+				$data["name"]="SampleTribe";
+				$data["loan_type"]=0;		    
+				//remove above hard coded parameter later
+				$business_pan=isset($data['company_pan_card'])?$data['company_pan_card']:$data['business_run_by_pan_div'];
+				$data['online_sale_channel']=isset($data['online_sale_channel'])?$data['online_sale_channel']:"";
+				$id=$data['online_ids'];
+				
+				print_r($data['online_ids_array']);exit();
 				$data['middle_name']=isset($data['middle_name'])?$data['middle_name']:'""';
 				$data['ownership']=isset($data['ownership'])?$data['ownership']:0;
 				$data['agent_name']=isset($data['agent_name'])?$data['agent_name']:'""';
 				$data['social']=isset($data['social'])?$data['social']:'""';
-				$data['partners_count']=isset($data['partners_count'])?$data['partners_count']:0;
-				$data['directors_count']=isset($data['directors_count'])?$data['directors_count']:0;
-				$data['proprietorship_type']=isset($data['proprietorship_type'])?$data['proprietorship_type']:'""';
+				$data['partners_count']=isset($data['partners_count'])?$data['partners_count']:'""';
+				$data['directors_count']=isset($data['directors_count'])?$data['directors_count']:'""';
+				//$data['proprietorship_type']=isset($data['proprietorship_type'])?$data['proprietorship_type']:'""';
 				$data['taxation_details']=isset($data['taxation_details'])?$data['taxation_details']:'';
 				$data['online_sale_channel']=isset($data['online_sale_channel'])?$data['online_sale_channel']:'""';
-				$data['business_run_by']=isset($data['business_run_by'])?$data['business_run_by']:0;
+				$data['business_run_by']=isset($data['business_run_by'])?$data['business_run_by']:'""';
 				$data['business_run_by_pan_div']=isset($data['business_run_by_pan_div'])?$data['business_run_by_pan_div']:'""';
-
+				print_r($data);exit();
 				$post_data='{"agentname":"'.$data['agent_name'].'",
 		"business_details":{
-			"address":[{
+			"address":{
 				"work":"'.$data['agent_name'].'"
-			}],
+			},
 			"director_count":'.$data['directors_count'].',
 			"employess_count":"'.$data['employee_count'].'",
 			"is_offline_sales":'.$data['offline_sale'].',
@@ -73,26 +81,21 @@ class tribeController extends CallApiController
 			"loan_purpose":'.$data['loan_purpose'].',
 			"nature_of_business":'.$data['business_type'].',
 			"online_sales_channels":['.$data['online_sale_channel'].'],
-			"ownership":'.$data['owner_name'].',
-			"pan_number":'.$data['business_run_by_pan_div'].',
+			"ownership":'.$data['business_premises'].',
+			"pan_number":"'.$business_pan.'",
 			"partner_count":'.$data['partners_count'].',
 			"product_sell":['.$data['selling_product'].'],
 			"proprietorship_type":'.$data['business_run_by'].',
 			"registration_details":'.$data['registration_detail'].',
-			"taxation_details":["'.$data['taxation_details'].'"],
+			"taxation_details":{'.$data['taxation_details'].'},
 			"turnover":'.$data['turnover'].'
 		},
-		"documents":[{
-			"data":"'.$base64.'",
-			"title":"'.$documents_array[$i].'",
-			"type":"'.$extension.'"
-		}],
 		"is_loan_distributor":true,
 		"loan_amount":"'.$data['loan_amount'].'",
 		"loan_tenure":'.$data['loan_tenure'].',
-		"loan_type":'.$data['agent_name'].',
+		"loan_type":'.$data['loan_type'].',
 		"loandistributorid":"'.$data['agent_name'].'",
-		"name":"'.$data['agent_name'].'",
+		"name":"'.$data['name'].'",
 		"online_ids":[{
 			"aggregated_type":'.$data['online_ids'].',
 			"credential":{
@@ -109,9 +112,9 @@ class tribeController extends CallApiController
 		"personal_details":{
 			"DOB":"'.$data['dob'].'",
 			"aadhaar_number":"'.$data['aadhar_no'].'",
-			"address":[{
+			"address":{
 				"work":"'.$data['address'].'"
-			}],
+			},
 			"education":'.$data['education'].',
 			"email":"'.$data['email'].'",
 			"family_details":'.$data['family_detail'].',

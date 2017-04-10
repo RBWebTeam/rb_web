6d7ac6254a9cccc816f4ca8620045842dfaee9b4
manishdixit18 <manishdixit18@gmail.com>
2017-04-10 10:55:16 +0530

    Auto commit module

--- resources/views/test_parse.blade.php
+++ resources/views/test_parse.blade.php
@@ -1,95 +1,142 @@
 <!DOCTYPE html>
-<html ng-app="plunker">
+<html>
 
-<head>
-<meta charset="utf-8">
-<title>Leaflet</title>
-<script>
-  document.write('<base href="' + document.location + '" />');
-</script>
-<link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css">
-<link rel="stylesheet"  href="style.css" />
-
-<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-<script type="text/javascript" src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"></script>
-</head>
- 
-<body  ng-controller="NavCtrl">
-
-  <div class="navbar">
-    <ul class="nav navbar-nav">
-      <li ng-class="navClass('home')"><a href='' ng-click="loadHome()">Home</a></li>
-      <li ng-class="navClass('about')"><a href='' ng-click="loadAbout()">About Us</a></li>
-      <li ng-class="navClass('contact')"><a href='' ng-click="loadContact()">Contact Us</a></li>
-    </ul>
-  </div>
+  <head>
+    <meta charset="utf-8"/>
   
-  <div ng-view></div>
+
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet" />
+
+    <!-- The main CSS file -->
+    <!-- <link href="style.css" rel="stylesheet" /> -->
+
+    <!--[if lt IE 9]>
+      <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+
+  <!-- The ng-app directive tells angular that the code below should be evaluated -->
+
+  <body ng-app>
+
+    <!-- The navigation menu will get the value of the "active" variable as a class.
+       The $event.preventDefault() stops the page from jumping when a link is clicked. -->
+
+    <nav class="@{{active}}" ng-click="$event.preventDefault()" ng-init="active='BASIC DETAIL'">
+
+      <!-- When a link in the menu is clicked, we set the active variable -->
+    <a href="#" id="nav0" ng-click="active='BASIC DETAIL'" >BASIC DETAIL</a>
+    <a href="#" ng-click="active='LOAN DETAIL'" id="nav1" class="go_to_next"><b>LOAN DETAIL</a>
+    <a href="#" ng-click="active='PERSONAL DETAIL'" id="nav2" class="go_to_next"><b>PERSONAL DETAIL</a>
+    <a href="#" ng-click="active='BUSINESS DETAIL'" id="nav3" class="go_to_next"><b>BUSINESS DETAIL</a>
+    <a href="#" ng-click="active='ONLINE ID'" id="nav5" class="go_to_next"><b>ONLINE ID</a>
+    <a href="#" ng-click="active='REFERENCE'" id="nav6" class="go_to_next"><b>REFERENCE</a>
+    <a href="#"  id="nav4" ng-click="active='DOCUMENT'" class="go_to_next"><b>DOCUMENT</a>
+    <a href="#"  id="nav7" ng-click="active='BANK STATEMENT'" class="go_to_next"><b>BANK STATEMENT</a>
 
 
-</body>
-<script type="text/javascript">
-  var app = angular.module("plunker", []).
-  config(function ($routeProvider, $locationProvider, $httpProvider) {
+    </nav>
 
-    $routeProvider.when('/home',
-    {
-      templateUrl:    'home.html',
-      controller:     'HomeCtrl'
-    });
-    $routeProvider.when('/about',
-    {
-      templateUrl:    'about.html',
-      controller:     'AboutCtrl'
-    });
-    $routeProvider.when('/contact',
-    {
-      templateUrl:    'contact.html',
-      controller:     'ContactCtrl'
-    });
-    $routeProvider.otherwise(
-    {
-      redirectTo:     '/home',
-      controller:     'HomeCtrl', 
-    }
-  );
-});
+    <!-- ng-show will show an element if the value in the quotes is truthful,
+       while ng-hide does the opposite. Because the active variable is not set
+       initially, this will cause the first paragraph to be visible. -->
 
-app.controller('NavCtrl', 
-['$scope', '$location', function ($scope, $location) {
-  $scope.navClass = function (page) {
-    var currentRoute = $location.path().substring(1) || 'home';
-    return page === currentRoute ? 'active' : '';
-  };
-  
-  $scope.loadHome = function () {
-        $location.url('/home');
-    };
-    
-      $scope.loadAbout = function () {
-        $location.url('/about');
-    };
-    
-      $scope.loadContact = function () {
-        $location.url('/contact');
-    };
-    
-}]);
 
-app.controller('AboutCtrl', function($scope, $compile) {
-  console.log('inside about controller');
+    <p ng-show="active">You chose <b>@{{active}}</p>
 
-});
-
-app.controller('HomeCtrl', function($scope, $compile) {
-  console.log('inside home controller');
-
-});
-
-app.controller('ContactCtrl', function($scope, $compile) {
-  console.log('inside contact controller');
-
-});
-</script>
+    <!-- Include AngularJS from Google's CDN -->
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"></script>
+  </body>
 </html>
+<style type="text/css">
+  *{
+  margin:0;
+  padding:0;
+}
+
+
+/*-------------------------
+  General Styles
+--------------------------*/
+
+
+body{
+  font:15px/1.3 'Open Sans', sans-serif;
+  color: #5e5b64;
+  text-align:center;
+}
+
+a, a:visited {
+  outline:none;
+  color:#389dc1;
+}
+
+a:hover{
+  text-decoration:none;
+}
+
+section, footer, header, aside, nav{
+  display: block;
+}
+
+
+/*-------------------------
+  The menu
+--------------------------*/
+
+
+nav{
+  display:inline-block;
+  margin:60px auto 45px;
+  background-color:#ec1b23;
+  box-shadow:0 1px 1px #ccc;
+  border-radius:2px;
+}
+
+nav a{
+  display:inline-block;
+  padding: 18px 30px;
+  color:#fff !important;
+  font-weight:bold;
+  font-size:14px;
+  text-decoration:none !important;
+  line-height:1;
+  text-transform: uppercase;
+  background-color:transparent;
+
+  -webkit-transition:background-color 0.25s;
+  -moz-transition:background-color 0.25s;
+  transition:background-color 0.25s;
+}
+
+nav a:first-child{
+  border-radius:2px 0 0 2px;
+}
+
+nav a:last-child{
+  border-radius:0 2px 2px 0;
+}
+
+nav.home .home,
+nav.team.team,
+nav.about .about,
+nav.contact .contact{
+  background-color:#5867E3;
+}
+
+p{
+  font-s
+  color:#7d9098;
+}
+
+p b{
+  color:#ffffff;
+  display:inline-block;
+  padding:5px 10px;
+  background-color:#DC64C6;
+  border-radius:2px;ize:22px;
+  font-weight:bold;
+  text-transform:uppercase;
+  font-size:18px;
+}
+</style>

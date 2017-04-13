8a78ccda54a9af13149b70be236a773c4ebf43cc
joeljangam <joeljangam146@gmail.com>
2017-04-13 11:36:21 +0530

    changes on index,header and web

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -137,12 +137,12 @@
 						<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" data-target="#">SME Loan</a>
 						 <ul class="dropdown-menu">
 						     <li>
-								<a href="{{URL::to('new-business-loan')}}">Unsecured Business Loan</a>
-								<a href="{{URL::to('new-loan-against-property')}}">Loan Against Property</a>
-								<a href="{{URL::to('new-loan-against-property?commercial')}}">Commercial Property Purchase</a>
+								<a href="{{URL::to('business-loan')}}">Unsecured Business Loan</a>
+								<a href="{{URL::to('loan-against-property')}}">Loan Against Property</a>
+								<a href="{{URL::to('loan-against-property?commercial')}}">Commercial Property Purchase</a>
 							</li>
 							<li>
-								<a href="{{URL::to('new-loan-against-property')}}">Lease Rent Discounting</a>
+								<a href="{{URL::to('loan-against-property')}}">Lease Rent Discounting</a>
 								<a href="{{URL::to('new-working-capital')}}">Working Capital</a>
 								<a href="{{URL::to('contact-us')}}">FCNR Loans</a>
 								<a href="{{URL::to('contact-us')}}">Debt Restructuring </a>
@@ -159,12 +159,12 @@
 						<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" data-target="#">Retail Loan</a>
 						<ul class="dropdown-menu">
 						   <li>
-								<a href="{{URL::to('new-home-loan')}}">Home Loan</a>
-								<a href="{{URL::to('new-smart-home-loan')}}">Smart Home Loan</a>
+								<a href="{{URL::to('home-loan')}}">Home Loan</a>
+								<a href="{{URL::to('smart-home-loan')}}">Smart Home Loan</a>
 								
-								<a href="{{URL::to('new-loan-against-property')}}">Loan Against Property</a>
-								<a href="{{URL::to('new-personal-loan')}}">Personal Loan</a>
-								<a href="{{URL::to('new-car-loan')}}">Car Loan</a>
+								<a href="{{URL::to('loan-against-property')}}">Loan Against Property</a>
+								<a href="{{URL::to('personal-loan')}}">Personal Loan</a>
+								<a href="{{URL::to('car-loan')}}">Car Loan</a>
 								<a href="{{URL::to('used-car-loan')}}">Used Car Loan </a>
 
 						   </li>

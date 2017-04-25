afdfa5f3ee297e403b2266a4ba84a309c5d096ac
gadesg666 <shekhar.gade@rupeeboss.com>
2017-04-25 15:26:15 +0530

    changes in Responsive view

--- resources/views/new-personal-loan.blade.php
+++ resources/views/new-personal-loan.blade.php
@@ -127,7 +127,7 @@
               <a class="btn btn-primary btn-outline with-arrow top-mrg product_name" id="btn_refresh_co1" data-toggle="modal" data-target="#login_process">Get Best Quotes<i class="icon-arrow-right"></i></a>
             <?php } ?> -->
 
-
+<p id="err" style="display:none;" ><span style="color: red;font-size: 16px;display: block; text-align: center;">Sorry, no quotes found for your given requirements.</span></p>
              <a class="btn btn-primary btn-outline with-arrow top-mrg product_name product_ID"   >Get Best Quotes<i class="icon-arrow-right"></i></a> 
 
  
@@ -182,7 +182,7 @@
        
 
     </div> 
-    <p id="err" style="display:none;" ><span style="color: red;font-size: 20px;display: block; text-align: center;">Sorry, no quotes found for your given requirements.</span></p> 
+     
   </div> 
    </aside>
    

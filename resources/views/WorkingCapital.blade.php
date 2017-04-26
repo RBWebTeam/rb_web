9fae27336055dc6dc7ab888852efeccd1bb07e70
durga pratap <scriptdp@gmail.com>
2017-04-26 11:22:24 +0530

    pull

--- resources/views/WorkingCapital.blade.php
+++ resources/views/WorkingCapital.blade.php
@@ -356,6 +356,9 @@
 
 
  
+ <input type="hidden" name="empid" class="empid" value=" <?php echo Session::get('empid')?Session::get('empid'):'';?>">
+<input type="hidden" name="brokerid" class="brokerid" value="<?php echo Session::get('brokerid')?Session::get('brokerid'):'';?>">
+<input type="hidden" name="source" class="source" value="<?php echo Session::get('source')?Session::get('source'):'';?>"> 
 
 
 @include('layout.footer')

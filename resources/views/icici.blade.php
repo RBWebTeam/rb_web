c950e5738ed60bfb9a6ff7484be0019c6290b5ba
joeljangam <joeljangam146@gmail.com>
2017-05-31 17:24:55 +0530

    changes on icici

--- resources/views/icici.blade.php
+++ resources/views/icici.blade.php
@@ -17,7 +17,7 @@
 <h3>Net Annual Income</h3>
    <div class="btn-grp mrg-top income-tab" data-toggle="buttons" id="income">
       <span class="btn btn-default outer-brd btn-block active">
-     <input type="radio"><a id="hide" onclick="set_annual_income('>2.5Lacs')">> 2.5 Lacs</a>
+     <input type="radio"><a onclick="set_annual_income('>2.5Lacs')">> 2.5 Lacs</a>
       </span>
       <span class="btn btn-default outer-brd btn-block ">
      <input type="radio"><a onclick="set_annual_income('>5.0Lacs')">>5.0 Lacs</a></span>
@@ -32,8 +32,8 @@
 <div class="col-md-8">
 <ul class="nav nav-tabs nav-justified" id="interest">
   <li class="active"><a data-toggle="tab" href="#home" onclick="set_interest('Lifestyle')">Lifestyle</a></li>
-  <li><a style="display: none;" data-toggle="tab" id="travel" href="#menu1" onclick="set_interest('Travel')">Travel</a></li>
-  <li><a style="display: none;" data-toggle="tab" id="motorsports" href="#menu2" onclick="set_interest('Motorsports')">Motorsports</a></li>
+  <li><a data-toggle="tab" href="#menu1" onclick="set_interest('Travel')">Travel</a></li>
+  <li><a data-toggle="tab" href="#menu2" onclick="set_interest('Motorsports')">Motorsports</a></li>
 </ul>
 
 <div class="tab-content text-left">
@@ -89,7 +89,7 @@
 
 <!-- 5.0 -->
 <div class="tab-content text-left">
-  <div id="home1_lifestyle_5" style="display: none;" >
+  <div id="home1_lifestyle_5" style="display: none;" class="tab-pane fade in active">
   <div class="col-md-4">
   <img src="images/CS/RD/DA_Coral-Mastercard-MasterCard_1.jpg" width="230" class="pull-left card-img"/>
   </div>
@@ -287,18 +287,6 @@
   { 
        income=value;
        show_card();
-        if ( income == '>2.5Lacs')
-      {
-        
-        $("#travel").hide();
-        $("#motorsports").hide();
-       
-      }else if(income == '>5.0Lacs' || income == '>10Lacs' || income == '>15Lacs')
-      {
-        
-        $("#travel").show();
-        $("#motorsports").show();
-      }
   }
   function set_interest(value)
   { 

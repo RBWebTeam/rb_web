362a55597c574aeac3e4a8c5a4fb6e61978efbf8
joeljangam <joeljangam146@gmail.com>
2017-06-19 17:56:55 +0530

    changes on iifl

--- resources/views/apply-iifl-loan.blade.php
+++ resources/views/apply-iifl-loan.blade.php
@@ -412,7 +412,7 @@
                         <path d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0"/>
                     </svg>
                 </span>
-                <span class="input input--nao">
+                <span class="input input--nao input--filled">
                     <select class="input__field input__field--nao" name="CurrentCity" id="CurrentCity" required>
                     <option disabled selected value=""></option>
                       
@@ -479,7 +479,7 @@
                         <path d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0"/>
                     </svg>
                 </span>
-                <span class="input input--nao">
+                <span class="input input--nao input--filled">
                     <select class="input__field input__field--nao" name="PermanentCity" id="PermanentCity" required>
                     <option disabled selected value=""></option>
                       

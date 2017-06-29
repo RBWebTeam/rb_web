cccb9e89bcf07b8f3c43316df979b3e930d2f857
pratik.telang <pratik.telang@rupeeboss.com>
2017-06-29 11:47:18 +0530

    Pull Data

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -18,7 +18,7 @@
 
 	<link  rel="stylesheet" type="text/css" href="{{URL::to('css/mysite.css')}}"/>
 
-  <link  rel="stylesheet" type="text/css" href="{{URL::to('css/style.css')}}"/>
+  <link  rel="stylesheet" type="text/css" href="{{URL::to('css/style.css')}}"/> 
 
 
 

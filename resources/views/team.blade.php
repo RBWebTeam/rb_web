80b330fae06ac25642e7cf54c38001f6860b267a
joeljangam <joeljangam146@gmail.com>
2017-07-14 15:51:32 +0530

    changes on team blade

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -1,29 +1,42 @@
 @include('layout.header')
 <br>
 <div class="container" id="fh5co-hero">
+<h2 class="loan-head" style="color:#85C1E9">Sticks in a bundle are unbreakable-The friendly team that makes it all happen</h2>
 
         <div class="col-sm-3">
             
-            <img src="{{URL::to('images/contact_img.jpg')}}" class="img-circle" alt="Cinque Terre" width="275" height="236">
-            <p style="text-align: center">Navin Kulkarni<br>
-               Co-founder<br> 
-             Looks after sales and UX design</p>
+            <img src="{{URL::to('images/pratik_telang.jpg')}}" class="img-circle" alt="Cinque Terre" width="255" height="256.13" >
+            <p style="text-align: center">Pratik Telang<br>
+               Senior Front-end Developer <br> 
+              Looks after sales and UX design</p>
         </div>
 
         <div class="col-sm-3">
-            <img src="{{URL::to('images/doll.jpg')}}" class="img-circle" alt="Cinque Terre" width="275" height="236">
+            <img src="{{URL::to('images/joel_jangam.jpg')}}" class="img-circle img-responsive" alt="Cinque Terre" width="275" height="236">
+            <p style="text-align: center">Joel Jangam<br>
+               Front-end Developer<br> 
+              Always curious for knowing and working on new user interface patterns</p>
         </div>
         
         <div class="col-sm-3">
-            <img src="{{URL::to('images/image_girl.jpg')}}" class="img-circle" alt="Cinque Terre" width="275" height="236">
+            <img src="{{URL::to('images/image_girl.jpg')}}" class="img-circle img-responsive" alt="Cinque Terre" width="275" height="236">
+            <p style="text-align: center">Navin Kulkarni<br>
+               Co-founder<br> 
+              Looks after sales and UX design</p>
         </div>
 
         <div class="col-sm-3">
-            <img src="{{URL::to('images/images_team.jpg')}}" class="img-circle" alt="Cinque Terre" width="275" height="236">
+            <img src="{{URL::to('images/manish_dixitji.jpg')}}" class="img-circle" alt="Cinque Terre" width="255" height="256.13">
+            <p style="text-align: center">Manish Dixit<br>
+               Senior Front-end Developer<br> 
+              Looks after sales and UX design</p>
         </div>
 
         <div class="col-sm-3">
             <img src="{{URL::to('images/contact_img.jpg')}}" class="img-circle" alt="Cinque Terre" width="275" height="236">
+            <p style="text-align: center">Navin Kulkarni<br>
+               Co-founder<br> 
+              Looks after sales and UX design</p>
         </div>
         
         

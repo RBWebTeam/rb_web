21a353a6617f5da864e69e868c73a879abb0b6dc
joeljangam <joeljangam146@gmail.com>
2017-07-19 14:25:45 +0530

    changes on icici credit card for dc

--- resources/views/thank-you.blade.php
+++ resources/views/thank-you.blade.php
@@ -2,7 +2,7 @@
 <div id="fh5co-hero">
 	<div class="container" id="elem">
 	<div class="col-md-12 pad centeralign" style="margin:104px 30px 30px -46px" >
-<center><img src="{{URL::to('images/thank_you.jpg')}}" class="img-responsive" ></center>
+<center><img src="{{URL::to('images/thank-you.jpg')}}" class="img-responsive" ></center>
 </div>
 </div>
 </div>

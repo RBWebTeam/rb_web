b671671a5127be7c20978123876a906caa5a8f3a
joeljangam <joeljangam146@gmail.com>
2017-07-19 14:26:31 +0530

    changes on icici credit card for dc

--- resources/views/thank-you.blade.php
+++ resources/views/thank-you.blade.php
@@ -1,7 +1,7 @@
 @include('layout.header')
 <div id="fh5co-hero">
 	<div class="container" id="elem">
-	<div class="col-md-12 pad centeralign" style="margin:104px 30px 30px -46px" >
+	<div class="col-md-12 pad centeralign"  >
 <center><img src="{{URL::to('images/thank-you.jpg')}}" class="img-responsive" ></center>
 </div>
 </div>
